44560c2d8801cf22acb99a9f3aca9c1c2add7d51
kelseyleewerner <kelsey.lee.werner@gmail.com>
2018-11-25 12:23:33 -0800

    Connect app with mysql database

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'localmysql',
+  database : 'pom_tracker'
+});
+
+
+
+
 
 app.use(express.static('dist'))
 
 app.get('/api/v1/pomeranians', (req, res) => {
-  res.send([{
-    website: 'pomadoption.com',
-    breed: 'Pomeranian',
-    name: 'Joe',
-    age: 5,
-    details: 'Joe is v friendly and loves treats'
-  }])
+  connection.connect();
+
+  connection.query('SELECT * from pomeranians', (error, results, fields) => {
+    if (error) throw error;
+    res.send(results)
+  });
+
+  connection.end();
+
+  // res.send([{
+  //   website: 'pomadoption.com',
+  //   breed: 'Pomeranian',
+  //   name: 'Joe',
+  //   age: 5,
+  //   details: 'Joe is v friendly and loves treats'
+  // }])
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
